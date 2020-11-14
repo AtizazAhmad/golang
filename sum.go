db2f5c0c86c52d8d0ae52d94079e566ea40a968b
someone <someone@someplace.com>
2020-11-14 12:57:53 +0500

    sum of numbers

---
+++ sum.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"  
+
+func main() {
+var n,sum,x int
+fmt.Println("Enter number of integers :")
+fmt.Scanf("%d",&n)
+fmt.Println("Enter the Integer whose sum You want")
+for i:=1;i<=n;i++{
+fmt.Scanf("%d",&x)
+sum+=x
+}
+fmt.Println("The Sum of Above Intgers") 
+fmt.Println(sum)}
